664d19beb68027e997270128ebb17a497cb8572b
Umberto Ponci <u.ponci@gmail.com>
2026-02-08 23:36:21 +0100

    added things

--- cpp_00/ex01/main.cpp
+++ cpp_00/ex01/main.cpp
@@ -3,20 +3,5 @@
 
 int main()
 {
-    PhoneBook phonebook;
-    std::string command;
 
-    while (true)
-    {
-        std::cout << "Enter command (ADD, SEARCH, EXIT): ";
-        std::getline(std::cin, command);
-
-        if (command == "ADD")
-            phonebook.addContact();
-        else if (command == "SEARCH")
-            phonebook.searchContacts();
-        else if (command == "EXIT")
-            break;
-    }
-    return 0;
 }
--- cpp_00/ex01/phonebook.hpp
+++ cpp_00/ex01/phonebook.hpp
@@ -3,52 +3,29 @@
 
 #include <iostream>
 #include <string>
-#include <iomanip>
 
-class Contact
-{
-private:
-    std::string first_name;
-    std::string last_name;
-    std::string nickname;
-    std::string phone_number;
-    std::string darkest_secret;
+class Contact {
+    private:
+        std::string FirstName;
+        std::string LastName;
+        std::string Nickname;
+        std::string PhoneNumber;
+        std::string DarkestSecret;
+        bool initialized;
+    
+    public:
+        Contact() : initialized(false) {};
+        void setContact(std::string first, std::string last, std::string nick, std::string phone, std::string secret);
+        
+        void printContact(); 
 
-public:
-    Contact();
+        int contactExists();
 
-    void setContact(const std::string& fn,
-                    const std::string& ln,
-                    const std::string& nn,
-                    const std::string& pn,
-                    const std::string& ds);
+        std::string getFirstName();
 
-    std::string getFirstName() const;
-    std::string getLastName() const;
-    std::string getNickname() const;
-    std::string getPhoneNumber() const;
-    std::string getDarkestSecret() const;
+        std::string getLastName();
 
-    bool isEmpty() const;
-};
-
-
-class PhoneBook
-{
-private:
-    Contact contacts[8];
-    int     count;
-    int     next_index;
-
-    std::string formatField(const std::string& str) const;
-
-public:
-    PhoneBook();
-
-    void addContact();
-    void searchContacts() const;
-};
-
-
+        std::string getNickName();
+}
 
 #endif
