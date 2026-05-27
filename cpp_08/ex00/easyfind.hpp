c3220bf7b36d4a442987bd1c2836a4717c9ad860
Umberto Ponci <u.ponci@gmail.com>
2026-05-27 08:11:08 +0200

    added 09 and tweaks to 08

--- cpp_08/ex00/easyfind.hpp
+++ cpp_08/ex00/easyfind.hpp
@@ -3,16 +3,16 @@
 #define EASYFIND_HPP
 
 #include <algorithm>
-#include <stdexcept>
+#include <stdexcept> //get rid of this and change to standard execption
 
 template <typename T>
-typename T::const_iterator easyfind(const T& container, int value)
+typename T::const_iterator easyfind(const T& container, int value)  //using reference bcs no modifiyng
 {
 	typename T::const_iterator it;
 
 	it = std::find(container.begin(), container.end(), value);
 	if (it == container.end())
-		throw std::out_of_range("easyfind: value not found");
+		throw std::out_of_range("easyfind: value not found"); //change exceotion to standard 
 	return it;
 }
 
--- cpp_08/ex01/main.cpp
+++ cpp_08/ex01/main.cpp
@@ -38,3 +38,6 @@ int main() {
     }
     return 0;
 }
+
+
+//improve clarity of tests clearly printing out what is which, which tests what for evaluators
